184867fd1abe4a7c8a50393b7db25c3ca127aed0
luansama <luansama2k13@gmail.com>
2026-09-24 21:29:26 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "study-reminder-v1";
+```javascript
+const CACHE_NAME = "study-reminder-v2";
 
 const FILES_TO_CACHE = [
     "./",
@@ -8,31 +9,103 @@ const FILES_TO_CACHE = [
     "./manifest.json"
 ];
 
+
+// Cài service worker mới
 self.addEventListener("install", function(event) {
 
     event.waitUntil(
+
         caches.open(CACHE_NAME)
             .then(function(cache) {
 
-                return cache.addAll(FILES_TO_CACHE);
+                return cache.addAll(
+                    FILES_TO_CACHE
+                );
 
             })
+
     );
+
+    // Kích hoạt ngay, không chờ tab cũ đóng
+    self.skipWaiting();
 
 });
 
 
+// Xóa cache phiên bản cũ
+self.addEventListener("activate", function(event) {
+
+    event.waitUntil(
+
+        caches.keys()
+            .then(function(cacheNames) {
+
+                return Promise.all(
+
+                    cacheNames.map(
+                        function(cacheName) {
+
+                            if (
+                                cacheName !== CACHE_NAME
+                            ) {
+
+                                return caches.delete(
+                                    cacheName
+                                );
+
+                            }
+
+                        }
+                    )
+
+                );
+
+            })
+
+    );
+
+    // Áp dụng service worker mới ngay
+    self.clients.claim();
+
+});
+
+
+// Lấy file mới từ mạng trước
+// Nếu mạng lỗi thì dùng cache
 self.addEventListener("fetch", function(event) {
 
     event.respondWith(
 
-        caches.match(event.request)
+        fetch(event.request)
             .then(function(response) {
 
-                return response || fetch(event.request);
+                // Lưu bản mới vào cache
+                const responseClone =
+                    response.clone();
+
+                caches.open(CACHE_NAME)
+                    .then(function(cache) {
+
+                        cache.put(
+                            event.request,
+                            responseClone
+                        );
+
+                    });
+
+                return response;
+
+            })
+
+            .catch(function() {
+
+                return caches.match(
+                    event.request
+                );
 
             })
 
     );
 
 });
+```
